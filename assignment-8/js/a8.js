52ca356593585f833ddca0d73cb5d3e86851e65c
E. Brink <brinkbrink@gmail.com>
2022-03-17 12:14:10 -0700

    v3

--- assignment-8/js/a8.js
+++ assignment-8/js/a8.js
@@ -48,3 +48,38 @@ $( function() {
     state = !state;
   });
 } );
+
+// progress bar function 1
+$( function() {
+  $( "#progressbar1" ).progressbar({
+    value: 20
+  });
+} );
+
+// progress bar function 2
+$( function() {
+  $( "#progressbar2" ).progressbar({
+    value: 40
+  });
+} );
+
+// progress bar function 3
+$( function() {
+  $( "#progressbar3" ).progressbar({
+    value: 60
+  });
+} );
+
+// progress bar function 4
+$( function() {
+  $( "#progressbar4" ).progressbar({
+    value: 80
+  });
+} );
+
+// progress bar function 5
+$( function() {
+  $( "#progressbar5" ).progressbar({
+    value: 100
+  });
+} );
